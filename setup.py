cd82f132154bc67a9965538ce7dded524fed4265
Ken Kundert <thenurd@nurdletech.com>
2015-07-06 09:26:15 -0700

    Added installer.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+from textwrap import dedent
+
+setup(
+    name='fixdisplays',
+    version='1.0',
+    description=dedent("Controls which screens are used"),
+    author="Ken Kundert",
+    author_email='theNurd@nurdletech.com',
+    scripts=['fixdisplays'],
+    license='GPLv3'
+)
+
+# vim: set sw=4 sts=4 et:
