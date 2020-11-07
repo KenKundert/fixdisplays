ebd817a59525d27934ce03c4426e529588297305
Ken Kundert <ken@theKunderts.net>
2020-11-06 20:56:14 -0800

    update list of supported python versions

--- setup.py
+++ setup.py
@@ -22,6 +22,9 @@ setup(
         'Natural Language :: English',
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Topic :: Utilities',
     ],
 )
